b38876ca89b4f3de5224765449cba60032d48b85
Kensley92 <72991374+Kensley92@users.noreply.github.com>
2021-01-29 21:22:25 -0500

    Create index.js

---
+++ Assignment 1 Debugging/index.js
@@ -0,0 +1,7 @@
+/// Loads Article.js
+const Article = require("./Article.js");
+/// Display Article Text in Console
+let article = new Article("Display");
+///Console log
+console.log(article.render() );
+
