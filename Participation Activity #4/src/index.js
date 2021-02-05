f6c5bd0a532151fcd5e276d289dc0d475016449a
Kensley92 <72991374+Kensley92@users.noreply.github.com>
2021-02-05 12:38:36 -0500

    Add files via upload

---
+++ Participation Activity #4/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+const customer = {
+
+  first_name: "Alex",
+
+  last_name: "DeVille"
+
+};
+
+//              your code inside these <div> tags
+
+const output = <div></div>;
+
+ReactDOM.render(output, document.querySelector("#root"));
